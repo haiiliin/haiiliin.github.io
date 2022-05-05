6e7cfb2bb721bc362884b57ca6d54fc01fb33393
haiiliin <haiiliin@users.noreply.github.com>
2022-05-05 04:49:08 +0000

    jekyll build from Action d6eaa0e850069a529dc812c0a6a1b19b911f25d2

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Intelligent Python Scripting in Abaqus",
+        "excerpt":"pyabaqus package pyabaqus is a Python package providing type hints for Python scripting of Abaqus, you can use it to write your Python script of Abaqus fluently, even without doing anything in Abaqus/CAE. GitHub repository: github.com/Haiiliin/pyabaqus PyPI: pyabaqus · PyPI Documentation: pyabaqus documentation Screenshots Build an Abaqus model Extract the...","categories": [],
+        "tags": ["Abaqus","Python"],
+        "url": "/abaqus-python/",
+        "teaser": null
+      },{
+        "title": "Soil-structure interface modeling with the nonlinear incremental approach",
+        "excerpt":"Abstract This paper aims to develop the nonlinear incremental modeling approach for describing both monotonic and cyclic behaviors of the soil-structure interface. An exponential function is adopted as an example to reproduce the asymptotic relationship between the interface shear stress ratio and the shear displacement. A stress-dilatancy relation is developed...","categories": ["Article","Simulation","Constitutive Model","Nonlinear Elastic Model","SSI","Optimization"],
+        "tags": ["Exponential","Critical State","Particle Breakage","Grain Size Distribution","Cyclic Loading","Fontainbleau Sand","Hostun Sand"],
+        "url": "/yang2021-nag/",
+        "teaser": null
+      },{
+        "title": "Modeling Monotonic and Cyclic Behavior of Granular Materials by Exponential Constitutive Function",
+        "excerpt":"Abstract It is still an open problem to develop a model with a uniform theoretical treatment under various loading conditions. This paper aims to formulate such a model for describing both monotonic and cyclic behaviors of granular materials. An exponential function was adopted to reproduce the stress-strain relation. Within the...","categories": ["Article","Simulation","Constitutive Model","Elastoplastic Model","Optimization"],
+        "tags": ["Exponential","Critical State","Particle Breakage","Cyclic Loading","Toyoura Sand"],
+        "url": "/yin2018-j-eng-mech/",
+        "teaser": null
+      },{
+        "title": "SANISAND: Simple anisotropic sand plasticity model",
+        "excerpt":"Abstract SANISAND is the name used for a family of simple anisotropic sand constitutive models developed over the past few years within the framework of critical state soil mechanics and bounding surface plasticity. The existing SANISAND models use a narrow open cone-type yield surface with apex at the origin obeying...","categories": ["Article","Simulation","Constitutive Model","Elastoplastic Model"],
+        "tags": ["SANISAND","Critical State","Bounding Surface","Toyoura Sand"],
+        "url": "/taiebat2008-nag/",
+        "teaser": null
+      },{
+        "title": "An elastoplastic constitutive relation for the soil-structure interface under cyclic loading",
+        "excerpt":"Abstract This paper includes a presentation of cyclic interface tests performed with smooth and rough surfaces on loose and dense silica sand under either a constant value of the normal stress or a zero normal displacement. These tests are then used for the elaboration of an elastoplastic constitutive relation based...","categories": ["Article","Simulation","Constitutive Model","Elastoplastic Model"],
+        "tags": ["Critical State","Bounding Surface","Cyclic Loading"],
+        "url": "/shahrour1997-cg/",
+        "teaser": null
+      }]
